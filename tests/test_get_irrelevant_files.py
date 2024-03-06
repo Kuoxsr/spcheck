1ea72ad2dd3866e43333d44b6e96e374a4feec84
Kuoxsr <kuoxsr@gmail.com>
2024-03-06 04:51:49 -0600

    Remove fake filesystem requirement in tests
    Eliminated the need for the fake filesystem objects in the tests for
    get_irrelevant_files, and did some general cleanup in each test, like
    declaring the files as paths rather than strings that need to be
    converted in multiple places later.
    
    Contributes to issue #10

--- tests/test_get_irrelevant_files.py
+++ tests/test_get_irrelevant_files.py
@@ -2,57 +2,45 @@ from objects.custom_path import Path
 from spcheck import get_irrelevant_files
 
 
-def test_get_irrelevant_files_should_return_list_when_files_are_irrelevant(fs):
+def test_get_irrelevant_files_should_return_list_when_files_are_irrelevant():
 
-    file1 = "/test/path/to/irrelevant/file.txt"
-    file2 = "/test/path/this/is/a/relevant/file.ogg"
-    file3 = "/test/path/another/irrelevant/file/.directory"
+    file1: Path = Path("/test/path/to/irrelevant/file.txt")
+    file2: Path = Path("/test/path/this/is/a/relevant/file.ogg")
+    file3: Path = Path("/test/path/another/irrelevant/file/.directory")
 
-    fs.create_file(file1)
-    fs.create_file(file2)
-    fs.create_file(file3)
-
-    all_files: list[Path] = [Path(file1), Path(file2), Path(file3)]
+    all_files: list[Path] = [file1, file2, file3]
 
     result = get_irrelevant_files(all_files)
 
     assert len(result) == 2
-    assert Path(file1) in result
-    assert Path(file3) in result
+    assert file1 in result
+    assert file3 in result
 
 
-def test_get_irrelevant_files_should_return_empty_list_when_files_are_relevant(fs):
+def test_get_irrelevant_files_should_return_empty_list_when_files_are_relevant():
 
-    file1 = "/relevant/file/in/a/different/path.ogg"
-    file2 = "/test/path/this/is/a/relevant/file.ogg"
-    file3 = "/test/path/relevant/file/testplan.ogg"
+    file1: Path = Path("/relevant/file/in/a/different/path.ogg")
+    file2: Path = Path("/test/path/this/is/a/relevant/file.ogg")
+    file3: Path = Path("/test/path/relevant/file/testplan.ogg")
 
-    fs.create_file(file1)
-    fs.create_file(file2)
-    fs.create_file(file3)
-
-    all_files: list[Path] = [Path(file1), Path(file2), Path(file3)]
+    all_files: list[Path] = [file1, file2, file3]
 
     result = get_irrelevant_files(all_files)
 
     assert len(result) == 0
 
 
-def test_get_irrelevant_files_should_be_sorted_alphabetically(fs):
+def test_get_irrelevant_files_should_be_sorted_alphabetically():
 
-    file1 = "/irrelevant/file/in/a/different/path.txt"
-    file2 = "/second/irrelevant_file/file.png"
-    file3 = "/another/irrelevant/file/.directory"
+    file1: Path = Path("/irrelevant/file/in/a/different/path.txt")
+    file2: Path = Path("/second/irrelevant_file/file.png")
+    file3: Path = Path("/another/irrelevant/file/.directory")
 
-    fs.create_file(file1)
-    fs.create_file(file2)
-    fs.create_file(file3)
-
-    all_files: list[Path] = [Path(file1), Path(file2), Path(file3)]
+    all_files: list[Path] = [file1, file2, file3]
 
     result = get_irrelevant_files(all_files)
 
     assert len(result) == 3
-    assert result[0] == Path(file3)
-    assert result[1] == Path(file1)
-    assert result[2] == Path(file2)
+    assert result[0] == file3
+    assert result[1] == file1
+    assert result[2] == file2
