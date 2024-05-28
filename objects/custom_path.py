6e395eb1b1f53581e72bb79d3fc016a3883063a2
Kuoxsr <kuoxsr@gmail.com>
2024-05-27 22:09:10 -0500

    Update python to 3.12 - improve CPath
    Had to upgrade to Python 3.12 anyway, since PyCharm update deleted 3.11
    and hosed my virtual environment. In the process, I discovered that a
    bug had been fixed regarding subclassing pathlib.Path, so I changed
    CPath to use __init__ instead of __new__

--- objects/custom_path.py
+++ objects/custom_path.py
@@ -1,9 +1,9 @@
 import pathlib
 
 
-class CPath(type(pathlib.Path())):
-    def __new__(cls, *args, **kwargs):
-        self = super().__new__(cls, *args, **kwargs)
+class CPath(pathlib.Path):
+    def __init__(self, *args):
+        super().__init__(*args)
 
         self.is_symbolic_link = False
         self.target_path = None
@@ -17,4 +17,19 @@ class CPath(type(pathlib.Path())):
             except FileNotFoundError:
                 self.target_path = None
 
-        return self
+    # def __new__(cls, *args, **kwargs):
+    #     self = super().__new__(cls, *args, **kwargs)
+    #
+    #     self.is_symbolic_link = False
+    #     self.target_path = None
+    #
+    #     # If the path is a symlink, store its target
+    #     # If it cannot be resolved, target is none
+    #     if self.is_symlink():
+    #         self.is_symbolic_link = True
+    #         try:
+    #             self.target_path = self.resolve()
+    #         except FileNotFoundError:
+    #             self.target_path = None
+    #
+    #     return self
